59c3fa795ac69359183cf34ea84766cbd48493da
lMeGoGol <megogo70444@gmail.com>
2025-11-21 17:24:36 +0200

    feat: implement passages (tunnels) for Pac-Man teleportation

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -7,6 +7,7 @@ public class Pacman : MonoBehaviour, @PlayerController.IGameplayActions
     private Movement movement;
     private PlayerController input;
     private Vector2 moveInput;
+    private Vector2 lastDirection;
 
     private void Awake()
     {
@@ -23,6 +24,17 @@ public class Pacman : MonoBehaviour, @PlayerController.IGameplayActions
     private void OnDisable()
     {
         input.Disable();
+    }
+
+    private void Update()
+    {
+        if (movement.direction != lastDirection)
+        {
+            float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+            this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+
+            lastDirection = movement.direction;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -45,8 +57,5 @@ public class Pacman : MonoBehaviour, @PlayerController.IGameplayActions
         {
             movement.SetDirection(Vector2.right);
         }
-
-        float angle = Mathf.Atan2(movement.direction.y, movement.direction.x);
-        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
 }
